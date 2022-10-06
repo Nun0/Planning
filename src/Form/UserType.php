672a0c304954388eeea3954e94309674e03afa77
flo <florence.cite@gmail.com>
2022-10-06 11:08:45 +0200

    ameliorer le usertype

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\ColorType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,15 +15,15 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email',EmailType::class, ["label" => "Email :"])
             ->add('roles', CollectionType::class , [
                 'allow_add' => true,
                 'allow_delete'=> true,
                 ])
-            ->add('password')
-            ->add('nom')
-            ->add('prenom')
-            ->add('telephone')
+            ->add('password',TextType::class, ["label" => "Password :"])
+            ->add('nom',TextType::class, ["label" => "Nom :"])
+            ->add('prenom',TextType::class, ["label" => "Prenom :"])
+            ->add('telephone',TextType::class, ["label" => "Telephone :"])
             ->add('couleur', ColorType::class)
         ;
     }
