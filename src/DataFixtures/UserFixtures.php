8679015303ed0a01e2b2c23b0d71dfeaa2e4eb35
flo <florence.cite@gmail.com>
2022-10-07 10:55:04 +0200

    ammeliorer crud

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,16 +2,16 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Symfony\Component\Security\Core\User\User;
 
 class UserFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
     {
-        // $user = new User();
+        $user = new User();
         // $user->setNom("Loto");
         // $user->setPrenom("Lutu");
         // $user->setTelephone("010101");
