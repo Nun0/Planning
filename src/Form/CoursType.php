d800e45abe96aac62f23667849222fcb32dfffc5
flo <florence.cite@gmail.com>
2022-10-10 14:43:52 +0200

    ajouter select2

--- src/Form/CoursType.php
+++ src/Form/CoursType.php
@@ -3,8 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Cours;
+use App\Entity\Promo;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class CoursType extends AbstractType
@@ -13,7 +15,17 @@ class CoursType extends AbstractType
     {
         $builder
             ->add('module')
-            ->add('promo')
+            ->add('promo', EntityType::class, [
+                'class' => Promo::class,
+                'required' => false,
+                'label' =>  'Promo(s) :',
+                'multiple' => true,
+                'by_reference' => true,
+                'attr' => [
+                    'class' => 'select2',
+                    'data-placeholder' => 'Promo(s) :',
+                ],
+            ])
         ;
     }
 
