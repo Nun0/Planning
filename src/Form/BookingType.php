27154b3e5dd2ce288fc75e36a039fb45a9dabb7c
Nuno <nunoribeiro.ep@gmail.com>
2022-10-21 12:33:54 +0200

    message

--- src/Form/BookingType.php
+++ src/Form/BookingType.php
@@ -18,6 +18,7 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\ColorType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
@@ -27,87 +28,94 @@ class BookingType extends AbstractType
     {
         $builder
             ->add('beginAt', DateType::class, [
-                'required'=> true,
                 'constraints' => [
-                    new NotBlank()
+                    new NotBlank(['message' => 'Ce champs ne peut pas être vide'])
                 ],
                 'label' => 'Date début',
                 'widget' => 'single_text',
                 'input' => 'datetime_immutable',
                 ])
-                ->add('endAt', DateType::class, [
-                    'required'=> true,
-                    'label' => 'Date fin',
-                    'widget' => 'single_text',
-                    'input' => 'datetime_immutable',
-                    ])
-                ->add('color', ColorType::class)
-                ->add('title')
-                ->add('formateur', EntityType::class, [
-                    'class' => User::class,
-                    'choice_label' => function($u){
-                        return $u->getPrenom() . ' ' . $u->getNom();
-                    },
-                    'required' => true,
-                    'query_builder' => function(UserRepository $ur){
-                        return $ur->createQueryBuilder('u')->orderBy('u.prenom', 'ASC');
-                    },
-                    'label' => 'Formateur: ',
-                    'placeholder' => 'selectionez le formateur... ',
-                    'multiple' => false,
-                    'by_reference' => true,
+
+            ->add('endAt', DateType::class, [
+                'label' => 'Date fin',
+                'widget' => 'single_text',
+                'input' => 'datetime_immutable',
                 ])
+            ->add('color', ColorType::class)
+            ->add('title', TextType::class, [
+                'constraints' => [
+                    new NotBlank(['message' => 'Ce champs ne peut pas être vide'])
+                ],
+            ])
+            ->add('formateur', EntityType::class, [
+                'class' => User::class,
+                'constraints' => [
+                    new NotBlank(['message' => 'Ce champs ne peut pas être vide'])
+                ],
+                'choice_label' => function($u){
+                    return $u->getPrenom() . ' ' . $u->getNom();
+                },
+                'required' => true,
+                'query_builder' => function(UserRepository $ur){
+                    return $ur->createQueryBuilder('u')->orderBy('u.prenom', 'ASC');
+                },
+                'label' => 'Formateur: ',
+                'placeholder' => 'selectionez le formateur... ',
+                'multiple' => false,
+                'by_reference' => true,
+            ])
 
-                ->add('centre', EntityType::class, [
-                    'class' => Centre::class,
-                    'required' => false,
-                    'label' => 'Centre: ',
-                    'placeholder' => 'selectionez le centre... ',
-                    'query_builder' => function(CentreRepository $cr){
-                        return $cr->createQueryBuilder('c')->orderBy('c.nom', 'ASC');
-                    },
+            ->add('centre', EntityType::class, [
+                'class' => Centre::class,
+                'required' => false,
+                'label' => 'Centre: ',
+                'placeholder' => 'selectionez le centre... ',
+                'query_builder' => function(CentreRepository $cr){
+                    return $cr->createQueryBuilder('c')->orderBy('c.nom', 'ASC');
+                },
+            ]);
+
+            $formModifier = function (FormInterface $form, Centre $centre = null) {
+                $promos = null === $centre ? [] : $centre->getPromos();
+    
+                $form->add('promo', EntityType::class, [
+                    'class' => Promo::class,
+                    'placeholder' => 'selectionez la promo...',
+                    'choices' => $promos,
                 ]);
+            };
 
-                $formModifier = function (FormInterface $form, Centre $centre = null) {
-                    $promos = null === $centre ? [] : $centre->getPromos();
-        
-                    $form->add('promo', EntityType::class, [
-                        'class' => Promo::class,
-                        'placeholder' => 'selectionez la promo...',
-                        'choices' => $promos,
-                    ]);
-                };
+            $builder->addEventListener(
+                FormEvents::PRE_SET_DATA,
+                function (FormEvent $event) use ($formModifier) {
+                    $data = $event->getData();
+                    $formModifier($event->getForm(), $data->getCentre());
+                }
+            );
 
-                $builder->addEventListener(
-                    FormEvents::PRE_SET_DATA,
-                    function (FormEvent $event) use ($formModifier) {
-                        $data = $event->getData();
-                        $formModifier($event->getForm(), $data->getCentre());
-                    }
-                );
+            $builder->get('centre')->addEventListener(
+                FormEvents::POST_SUBMIT,
+                function (FormEvent $event) use ($formModifier) {
+                    // It's important here to fetch $event->getForm()->getData(), as
+                    // $event->getData() will get you the client data (that is, the ID)
+                    $centre = $event->getForm()->getData();
+                    
+    
+                    // since we've added the listener to the child, we'll have to pass on
+                    // the parent to the callback function!
+                    $formModifier($event->getForm()->getParent(), $centre);
+                }
+            );
+    
+            // ->add('promo', ChoiceType::class, [
+            //     'class' => Promo::class,
+            //     'label' => 'Promo:',
+            //     'required' => true,
+            //     'placeholder' => 'selectionez la promo...',
+            //     'choice_filter' => function(){
 
-                $builder->get('centre')->addEventListener(
-                    FormEvents::POST_SUBMIT,
-                    function (FormEvent $event) use ($formModifier) {
-                        // It's important here to fetch $event->getForm()->getData(), as
-                        // $event->getData() will get you the client data (that is, the ID)
-                        $centre = $event->getForm()->getData();
-        
-                        // since we've added the listener to the child, we'll have to pass on
-                        // the parent to the callback function!
-                        $formModifier($event->getForm()->getParent(), $centre);
-                    }
-                );
-        
-                // ->add('promo', ChoiceType::class, [
-                //     'class' => Promo::class,
-                //     'label' => 'Promo:',
-                //     'required' => true,
-                //     'placeholder' => 'selectionez la promo...',
-                //     'choice_filter' => function(){
-
-                //     }
-                // ]);
+            //     }
+            // ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
